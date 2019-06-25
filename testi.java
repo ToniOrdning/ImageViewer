2835b121203c2610b7b4382a5ce5f4b0c350bbfb
ToniOrdning <tonior@uef.fi>
2019-06-26 00:13:17 +0300

    Manifest file and hotfixes.

--- testi.java
+++ testi.java
@@ -1,3 +1,12 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import javafx.application.Application;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
@@ -6,53 +15,65 @@ import javafx.stage.Stage;
 import javafx.stage.Window;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.geometry.Pos;
-import java.io.File;
-import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.RejectedExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.ScheduledExecutorService;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
 
-public class testi extends Application{
-    public static void main(String[] args){
+public class testi extends Application {
+    public static void main(String[] args) {
 
         launch(args);
 
     }
 
-    //Windowsize
-    private final double WINDOW_WIDTH = 1024; //Default window width
-    private final double WINDOW_HEIGHT = 768;  //Default window height
-    private double tempWindowWidth; //For resizing images
-    private double tempWindowHeight;//For resizing images
+    // Windowsize
+    private final double WINDOW_WIDTH = 1024; // Default window width
+    private final double WINDOW_HEIGHT = 768; // Default window height
+    private double tempWindowWidth; // For resizing images
+    private double tempWindowHeight;// For resizing images
 
-    //Misc
-    private List<File> selectedFiles;   //Selecting files with file manager
-    private int fileCount = 0;  //For randomizing slideshows
-    private BorderPane layout;    //Layout for program
-    private ImageView testImageView;    //Viewing images
-    private Image showNewImage; //switching image
+    // Misc
+    private List<File> selectedFiles; // Selecting files with file manager
+    private int fileCount = 0; // For randomizing slideshows
+    private BorderPane layout; // Layout for program
+    private ImageView showImageView; // Viewing images
+    private Image showNewImage; // switching image
     private Runnable slideshowImage = () -> {
         nextImage();
-    };   //For slideshows
+    }; // For slideshows
     private Runnable randomSlideshowImage = () -> {
         randomImage();
-    };  //For randomized slideshows
-    private int imageCounter = 0;   //Keeping track of shown image
-    private ScheduledExecutorService sessionChange =
-    Executors.newScheduledThreadPool(1);    //For timing picture changes
+    }; // For randomized slideshows
+    private int imageCounter = 0; // Keeping track of shown image
+    private ScheduledExecutorService sessionChange = Executors.newScheduledThreadPool(1);
+    // For timing picture changes
+    private Timer slideshowTimer = new Timer();
     Scene primaryScene;
 
-    //Menu
+    //Timers for slideshows
+    private TimerTask slideshowTimerTask = new TimerTask() {
+        @Override
+        public void run() {
+            nextImage();
+        }
+    };
+    private TimerTask randomSlideshowTimerTask = new TimerTask() {
+        @Override
+        public void run() {
+            nextImage();
+        }
+    };
+
+    // Menu
     private MenuBar defaultMenubar;
     private Menu fileMenu;
     private Menu startMenu;
@@ -65,93 +86,81 @@ public class testi extends Application{
     private MenuItem fullscreenMenuItem;
 
     @Override
-    public void start(Stage primaryStage){
+    public void start(Stage primaryStage) {
 
-        //Setup
+        // Setup
         primaryStage.setTitle("Image Viewer");
         layout = new BorderPane();
         primaryScene = new Scene(layout, WINDOW_WIDTH, WINDOW_HEIGHT);
 
-        //Image
-        testImageView = new ImageView();
-        testImageView.setFitWidth(WINDOW_WIDTH);
-        testImageView.setFitHeight(WINDOW_HEIGHT);
-        testImageView.setPreserveRatio(true);
+        // Image
+        showImageView = new ImageView();
+        showImageView.setFitWidth(WINDOW_WIDTH);
+        showImageView.setFitHeight(WINDOW_HEIGHT);
+        showImageView.setPreserveRatio(true);
 
-        //MenuItems
+        // MenuItems
         openMenuItem = new MenuItem("Open");
         openMenuItem.setOnAction(e -> {
-            chooseFile(testImageView);
+            chooseFile(showImageView);
         });
-        openMenuItem.setAccelerator(new KeyCodeCombination(
-            KeyCode.O, KeyCombination.CONTROL_DOWN
-        ));
+        openMenuItem.setAccelerator(new KeyCodeCombination(KeyCode.O, KeyCombination.CONTROL_DOWN));
         nextImageMenuItem = new MenuItem("Next");
         nextImageMenuItem.setOnAction(e -> {
             nextImage();
         });
-        nextImageMenuItem.setAccelerator(new KeyCodeCombination(
-            KeyCode.RIGHT, KeyCombination.CONTROL_ANY
-        ));
+        nextImageMenuItem.setAccelerator(new KeyCodeCombination(KeyCode.RIGHT, KeyCombination.CONTROL_ANY));
         previousImageMenuItem = new MenuItem("Previous");
         previousImageMenuItem.setOnAction(e -> {
             previousImage();
         });
-        previousImageMenuItem.setAccelerator(new KeyCodeCombination(
-            KeyCode.LEFT, KeyCombination.CONTROL_ANY
-        ));
+        previousImageMenuItem.setAccelerator(new KeyCodeCombination(KeyCode.LEFT, KeyCombination.CONTROL_ANY));
 
         startSlideshowMenuItem = new MenuItem("Slideshow");
         startSlideshowMenuItem.setOnAction(e -> {
             startSlideshow();
         });
-        startSlideshowMenuItem.setAccelerator(new KeyCodeCombination(
-            KeyCode.F9, KeyCombination.CONTROL_ANY
-        ));
+        startSlideshowMenuItem.setAccelerator(new KeyCodeCombination(KeyCode.F9, KeyCombination.CONTROL_ANY));
 
         randomSlideshowMenuItem = new MenuItem("Random Slideshow");
         randomSlideshowMenuItem.setOnAction(e -> {
             startRandomSlideshow();
         });
-        randomSlideshowMenuItem.setAccelerator(new KeyCodeCombination(
-            KeyCode.F10, KeyCombination.CONTROL_ANY
-        ));
+        randomSlideshowMenuItem.setAccelerator(new KeyCodeCombination(KeyCode.F10, KeyCombination.CONTROL_ANY));
 
         fullscreenMenuItem = new MenuItem("Fullscreen");
         fullscreenMenuItem.setOnAction(e -> {
-            if (primaryStage.isFullScreen() == false){
+            if (primaryStage.isFullScreen() == false) {
                 primaryStage.setFullScreen(true);
                 layout.getChildren().remove(defaultMenubar);
-            } else if (primaryStage.isFullScreen() == true){
+            } else if (primaryStage.isFullScreen() == true) {
                 primaryStage.setFullScreen(false);
-                layout.getChildren().remove(testImageView);
+                layout.getChildren().remove(showImageView);
                 layout.setTop(defaultMenubar);
-                layout.setCenter(testImageView);
+                layout.setCenter(showImageView);
             }
         });
         fullscreenMenuItem.setAccelerator(new KeyCodeCombination(KeyCode.F11));
 
-        //Menus
+        // Menus
         fileMenu = new Menu("File");
-        fileMenu.getItems().addAll(openMenuItem, nextImageMenuItem,
-        previousImageMenuItem);
+        fileMenu.getItems().addAll(openMenuItem, nextImageMenuItem, previousImageMenuItem);
 
         startMenu = new Menu("Start");
-        startMenu.getItems().addAll(startSlideshowMenuItem,
-        randomSlideshowMenuItem);
+        startMenu.getItems().addAll(startSlideshowMenuItem, randomSlideshowMenuItem);
 
         viewMenu = new Menu("View");
         viewMenu.getItems().addAll(fullscreenMenuItem);
 
-        //MenuBar
+        // MenuBar
         defaultMenubar = new MenuBar();
         defaultMenubar.getMenus().addAll(fileMenu, startMenu, viewMenu);
 
-        //Add elements
+        // Add elements
         layout.setTop(defaultMenubar);
-        layout.setCenter(testImageView);
+        layout.setCenter(showImageView);
 
-        //Listeners for window resizes
+        // Listeners for window resizes
         primaryScene.widthProperty().addListener((listener) -> {
             whenWindowSizeUpdate();
         });
@@ -159,24 +168,21 @@ public class testi extends Application{
             whenWindowSizeUpdate();
         });
 
-        //Final setup
+        // Final setup
         primaryStage.setScene(primaryScene);
         primaryStage.show();
 
     }
 
-    protected void chooseFile(ImageView viewer){   //Select files for viewing
+    protected void chooseFile(ImageView viewer) { // Select files for viewing
 
         imageCounter = 0;
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Choose your pictures");
-        fileChooser.setInitialDirectory(new File(System.getProperty("user.home"),
-        "//Pictures//"));
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home"), "//Pictures//"));
 
-        fileChooser.getExtensionFilters().addAll(
-            new ExtensionFilter("Image Files", "*.png", "*.jpg"),
-            new ExtensionFilter("PNG", "*.png"),
-            new ExtensionFilter("JPG", "*.jpg"));
+        fileChooser.getExtensionFilters().addAll(new ExtensionFilter("Image Files", "*.png", "*.jpg"),
+                new ExtensionFilter("PNG", "*.png"), new ExtensionFilter("JPG", "*.jpg"));
 
         selectedFiles = fileChooser.showOpenMultipleDialog(null);
 
@@ -184,7 +190,7 @@ public class testi extends Application{
 
     }
 
-    protected void nextImage(){
+    protected void nextImage() {
 
         if (imageCounter >= selectedFiles.size() - 1) {
             imageCounter = 0;
@@ -195,7 +201,7 @@ public class testi extends Application{
 
     }
 
-    protected void previousImage(){
+    protected void previousImage() {
 
         if (imageCounter <= 0) {
             imageCounter = selectedFiles.size() - 1;
@@ -205,8 +211,8 @@ public class testi extends Application{
         showNewImage();
 
     }
-    
-    protected void randomImage(){
+
+    protected void randomImage() {
 
         fileCount = selectedFiles.size();
         imageCounter = (int) Math.floor(Math.random() * fileCount + 1);
@@ -214,44 +220,51 @@ public class testi extends Application{
 
     }
 
-    protected void showNewImage(){
+    protected void showNewImage() {
 
         tempWindowWidth = primaryScene.getWidth();
         tempWindowHeight = primaryScene.getHeight();
 
-        try{
-            showNewImage = new Image("file:" + selectedFiles.get(imageCounter),
-            0, 0, true, false); //Get new image
+        try {
+            showNewImage = new Image("file:" + selectedFiles.get(imageCounter), 0, 0, true, false); // Get new image
         } catch (java.lang.NullPointerException e) {
             System.out.println(e.getMessage());
         }
 
         if (showNewImage.getWidth() > tempWindowWidth) {
-            testImageView.setFitWidth(tempWindowWidth);
+            showImageView.setFitWidth(tempWindowWidth);
         } else {
-            testImageView.setFitWidth(showNewImage.getWidth());
+            showImageView.setFitWidth(showNewImage.getWidth());
         }
         if (showNewImage.getHeight() > tempWindowHeight) {
-            testImageView.setFitHeight(tempWindowHeight);
+            showImageView.setFitHeight(tempWindowHeight);
         } else {
-            testImageView.setFitHeight(showNewImage.getHeight());
+            showImageView.setFitHeight(showNewImage.getHeight());
         }
 
-        testImageView.setSmooth(true);
+        showImageView.setSmooth(true);
 
-        testImageView.setImage(showNewImage);   //Show new image
-
-    }
-
-    protected void startSlideshow(){
-
-        sessionChange.scheduleAtFixedRate(slideshowImage, 1, 1, TimeUnit.SECONDS);
+        showImageView.setImage(showNewImage); // Show new image
 
     }
 
-    protected void startRandomSlideshow(){
+    protected void startSlideshow() {
 
-        sessionChange.scheduleAtFixedRate(randomSlideshowImage, 1, 1, TimeUnit.SECONDS);
+        try {
+            slideshowTimer.schedule(slideshowTimerTask, 1000, 1000);
+        } catch (IllegalStateException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    protected void startRandomSlideshow() {
+
+        try {
+            slideshowTimer.schedule(randomSlideshowTimerTask, 1000, 1000);
+        } catch (IllegalStateException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 
@@ -261,14 +274,14 @@ public class testi extends Application{
         tempWindowHeight = primaryScene.getHeight();
 
         if (showNewImage.getWidth() > tempWindowWidth) {
-            testImageView.setFitWidth(tempWindowWidth);
+            showImageView.setFitWidth(tempWindowWidth);
         } else {
-            testImageView.setFitWidth(showNewImage.getWidth());
+            showImageView.setFitWidth(showNewImage.getWidth());
         }
         if (showNewImage.getHeight() > tempWindowHeight) {
-            testImageView.setFitHeight(tempWindowHeight);
+            showImageView.setFitHeight(tempWindowHeight);
         } else {
-            testImageView.setFitHeight(showNewImage.getHeight());
+            showImageView.setFitHeight(showNewImage.getHeight());
         }
 
     }
